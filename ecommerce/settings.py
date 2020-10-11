c13b85e6af55121e941bfeeddb8d07291670a95c
Ismet <ismet_nasic@hotmail.com>
2020-10-11 23:31:02 +0200

    ahj

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
