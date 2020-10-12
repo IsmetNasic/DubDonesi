b6b2e50a6f17d29e12cafe36be4133d3bbaf9d6a
Ismet <ismet_nasic@hotmail.com>
2020-10-12 15:20:43 +0200

    agh

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Customer(models.Model):
 	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-	name = models.CharField(max_length=200, null=True)
+	username = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200)
 
 	def __str__(self):
