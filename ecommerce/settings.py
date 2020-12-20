d54437011f6531d55b3c76063111ccb955e9eb03
Ismet <ismet_nasic@hotmail.com>
2020-12-21 00:51:50 +0100

    aa

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DUBDONESI')
+SECRET_KEY = 'z+ksf@)0d^qojbh4rnp4b1to$hq&*tt(3bs$gf(3i267g$k9ln'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['dubdonesi.herokuapp.com']
 
